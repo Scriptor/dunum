fe3e55744972dfcc6914a39ba9d6e7ad768bf55a
Tamreen Khan <historium@gmail.com>
2016-09-16 09:15:09 -0400

    Rewrite in C

--- server.c
+++ server.c
@@ -1,17 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <netdb.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 
 #define SERVER_PORT 4242
+#define BUF_SIZE 256
+
+int accept_next(int fd) {
+    int comm_fd = accept(fd, (struct sockaddr*) NULL, NULL);
+    if (comm_fd < 0)
+        error("ERROR: Accepting");
+    return comm_fd;
+}
+
+int read_next(int fd, char *buf) {
+    int i, n;
+    char exp[4];
+    uint32_t num_expected;
+
+    bzero(exp, 4);
+    n = read(fd, exp, 4);
+    if (n < 0)
+        error("ERROR: Can't read number of expected bytes.");
+    else if (n == 0)
+        return 0;
+
+    num_expected = (exp[0] << 24) | (exp[1] << 16) | (exp[2] << 8) | exp[3];
+    num_expected = num_expected > BUF_SIZE ? BUF_SIZE : num_expected; 
+    printf("Expecting %d\n", num_expected);
+    
+    n = read(fd, buf, num_expected);
+    if (n < 0)
+        error("ERROR: Can't read data.");
+    printf("DATA: %s\n", buf);
+
+    return n;
+}
 
 int main() {
-    char buf[100];
     struct sockaddr_in servaddr;
-    int server_fd, comm_fd;
+    int n, listen_fd, comm_fd;
+    char buf[BUF_SIZE];
+    char output[BUF_SIZE];
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     bzero(&servaddr, sizeof(servaddr));
 
@@ -19,16 +53,24 @@ int main() {
     servaddr.sin_addr.s_addr  = htons(INADDR_ANY);
     servaddr.sin_port  = htons(SERVER_PORT);
 
-    bind(server_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
-    listen(server_fd, 10);
+    listen(listen_fd, 10);
 
-    comm_fd = accept(server_fd, (struct sockaddr*) NULL, NULL);
-
+    comm_fd = accept_next(listen_fd);
     while(1)
     {
-        bzero(buf, 100);
-        read(comm_fd, buf, 100);
-        write(comm_fd, buf, strlen(buf)+1);
+        bzero(buf, BUF_SIZE);
+        bzero(output, BUF_SIZE);
+        n = read_next(comm_fd, buf);
+        if (n > 0) {
+            sprintf(output, "%s\n", buf);
+            n = write(comm_fd, output, strlen(buf)+1);
+            if (n < 0)
+                error("ERROR: Can't write");
+        } else if(n == 0) {
+            close(comm_fd);
+            comm_fd = accept_next(listen_fd);
+        }
     }
 }
