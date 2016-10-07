f3c95759dc6024867fbd5786cdb73955d2d57c91
Tamreen Khan <historium@gmail.com>
2016-10-07 16:31:31 -0400

    Handle readers.
    Each reader can request N entries. It does so by sending the number
    of entries it wants (in big-endian) and receives the string back.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,11 @@ struct LogEntry<'a> {
     data: &'a [u8],
 }
 
+fn read_u32(buf: [u8; 4]) -> u32 {
+    let mut rdr = Cursor::new(buf);
+    rdr.read_u32::<BigEndian>().unwrap()
+}
+
 fn write_log(f: &File, entry: LogEntry) {
     let mut writer = BufWriter::new(f);
     writer.write_all(entry.size).unwrap();
@@ -21,7 +26,22 @@ fn write_log(f: &File, entry: LogEntry) {
     writer.flush().unwrap();
 }
 
-fn handle_client(stream: &mut TcpStream, log: &File) {
+fn read_entries(log: &mut File, n: u32)  -> Vec<String> {
+    let mut entries: Vec<String> = Vec::with_capacity(n as usize);
+    //let offset = 0;
+    for _ in 0..n {
+        let mut entry_size_buf = [0; 4];
+        let entry_size;
+        let mut data_buf = Vec::new();
+        log.read_exact(&mut entry_size_buf).unwrap();
+        entry_size = read_u32(entry_size_buf);
+        log.take(entry_size as u64).read_to_end(&mut data_buf).unwrap();
+        entries.push(String::from_utf8(data_buf).unwrap());
+    }
+    entries
+}
+
+fn handle_writer(stream: &mut TcpStream, log: &File) {
     let mut num_exp_buf;
     let mut data_buf = vec![];
     loop {
@@ -49,6 +69,22 @@ fn handle_client(stream: &mut TcpStream, log: &File) {
     }
 }
 
+fn handle_reader(stream: &mut TcpStream, log: &mut File) {
+    let mut num_entries_buf;
+    loop {
+        num_entries_buf = [0; 4];
+        match stream.read_exact(&mut num_entries_buf) {
+            Ok(_) => {
+                let num_entries = read_u32(num_entries_buf);
+                println!("reading {} entries", num_entries);
+                let entries = read_entries(log, num_entries);
+                let _ = stream.write(entries.join("").as_bytes());
+            }
+            _ => break
+        }
+    }
+}
+
 fn init_log() -> File {
     let f = OpenOptions::new()
             .append(true)
@@ -56,10 +92,10 @@ fn init_log() -> File {
     match f {
         Ok(f) => f,
         Err(_) => panic!("Error: Can't create the log file: {}!", LOG_FILE_NAME),
-    }
+    } 
 }
 
-fn get_log() -> File {
+fn get_log_writer() -> File {
     let f = OpenOptions::new()
             .read(true)
             .append(true)
@@ -70,21 +106,44 @@ fn get_log() -> File {
     } 
 }
 
+fn get_log_reader() -> File {
+    File::open(LOG_FILE_NAME).unwrap()
+}
+
 fn main(){
     let _ = init_log();
-    let listener = TcpListener::bind("0.0.0.0:4242").unwrap();
 
-    for s in listener.incoming() {
-        match s {
-            Ok(mut stream) => {
-                thread::spawn(move || {
-                    let f = get_log();
-                    handle_client(&mut stream, &f)
-                });
+    let writers_thread = thread::spawn(|| {
+        println!("Listening for writers on on :4242");
+        let writers = TcpListener::bind("0.0.0.0:4242").unwrap();
+        for s in writers.incoming() {
+            match s {
+                Ok(mut stream) => {
+                    thread::spawn(move || {
+                        let f = get_log_writer();
+                        handle_writer(&mut stream, &f)
+                    });
+                }
+                Err(e) => println!("ERROR! {:?}", e)
             }
-            Err(e) => println!("ERROR! {:?}", e)
         }
-    }
+    });
 
-    drop(listener)
+    let readers_thread = thread::spawn(|| {
+        println!("Listening for readers on on :2424");
+        let readers = TcpListener::bind("0.0.0.0:2424").unwrap();
+        for s in readers.incoming() {
+            match s {
+                Ok(mut stream) => {
+                    thread::spawn(move || {
+                        let mut f = get_log_reader();
+                        handle_reader(&mut stream, &mut f)
+                    });
+                }
+                Err(e) => println!("ERROR! {:?}", e)
+            }
+        }
+    });
+    let _ = writers_thread.join();
+    let _ = readers_thread.join();
 }
